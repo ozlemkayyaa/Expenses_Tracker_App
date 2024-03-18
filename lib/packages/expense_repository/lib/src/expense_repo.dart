be5979087ee51ee95fcd9fda0aeb5cc077bbb42c
ozlemkayyaa <ozlemkayyaa@hotmail.com>
2024-03-18 21:23:01 +0300

    Yorum satırları eklendi

--- lib/packages/expense_repository/lib/src/expense_repo.dart
+++ lib/packages/expense_repository/lib/src/expense_repo.dart
@@ -1,6 +1,10 @@
 import 'package:expense_repository/expense_repository.dart';
 
+//ExpenseRepository, harcama kategorileriyle ilgili işlemleri yapmak için arayüz sağlar.
 abstract class ExpenseRepository {
+  // Yeni bir kategori oluşturmayı sağlayan metot.
   Future<void> createCategory(Category category);
+
+  // Tüm kategorileri almayı sağlayan metot
   Future<List<Category>> getCategory();
 }
--- lib/packages/expense_repository/lib/src/firebase_expense_repo.dart
+++ lib/packages/expense_repository/lib/src/firebase_expense_repo.dart
@@ -10,6 +10,15 @@ class FirebaseExpenseRepo implements ExpenseRepository {
   final expenseCollection =
       FirebaseFirestore.instance.collection(Collections.expenses);
 
+  /*
+  Yeni bir kategori oluşturmayı sağlayan fonksiyon. Kategorinin Firestore veritabanına eklenmesi işlemini gerçekleştirir.
+  categoryCollection üzerinden kategori belgesi oluşturulur ve 
+  bu belge, kategori nesnesinin toEntity() metoduyla dönüştürülen Entity'e, 
+  daha sonra da toDocument() metoduyla dönüştürülen belgeye ayarlanır.
+  set() metodu kullanılarak Firestore'a yeni belge eklenir. 
+  Bu işlem sırasında hata oluşursa, log() ile hata mesajı kaydedilir ve hata yeniden atılır (rethrow).
+  */
+
   @override
   Future<void> createCategory(Category category) async {
     try {
@@ -21,6 +30,15 @@ class FirebaseExpenseRepo implements ExpenseRepository {
       rethrow;
     }
   }
+
+  /*  
+  Firestore'dan tüm kategorilerin alınmasını sağlayan fonksiyon. categoryCollection üzerinden tüm kategorilerin belgeleri getirilir.
+  Ardından, bu belgelerin içinden bir dizi kategori oluşturulur.
+  get() metodu asenkron olarak çalıştığı için, then() metodu kullanılarak belgelerin listesi alınır ve map() ile her bir belge,
+  CategoryEntity.fromDocument() ve sonrasında Category.formEntity() metotları kullanılarak kategori nesnesine dönüştürülür.
+  Son olarak, bu dönüştürülmüş kategori nesneleri bir listeye dönüştürülür ve döndürülür.
+  Herhangi bir hata durumunda, log() ile hata mesajı kaydedilir ve hata yeniden atılır (rethrow).
+  */
 
   @override
   Future<List<Category>> getCategory() async {
